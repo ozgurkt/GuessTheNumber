a250b3c54730d62d38795a4542edc708181477ef
ozgurkt <ozgurktaskesen@gmail.com>
2022-07-05 15:29:20 +0200

    Guess Number Final

--- src/test/java/GuessNumber/GN.java
+++ src/test/java/GuessNumber/GN.java
@@ -1,46 +1,46 @@
 package GuessNumber;
+
 import java.util.Random;
 import java.util.*;
+
 public class GN {
 
 
-          public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int number1;
-            Scanner input = new Scanner(System.in);
-            Random number = new Random();
-            int luckynumber = number.nextInt(51);
-            int count = 1000;
-            int times=0;
-            System.out.println("Guess the number from 1 to 50 with least error");
-            System.out.println("After each mistake you will lose 100 points");
-            //System.out.println(luckynumber);
+        int low = 10, high = 100, maxChance = 3, count = 0;
 
-            do {
-                System.out.println("guess the number now");
-                number1 = input.nextInt();
+        int number;
+        Scanner input = new Scanner(System.in);
+        Random guess = new Random();
+        int luckynumber = guess.nextInt(high - low) + low;
 
-                if (number1>luckynumber) {
-                    System.out.println("The number is smaller than you guessed");
-                }else if(number1<luckynumber) {
-                    System.out.println("The number is bigger than you guessed");
-                }
-                count= count - 100;
-                times ++;
-
-            }while (number1!=luckynumber);
-
-            System.out.println("Congratulations ! You got the number in " + times + " try!");
-            System.out.println("You won " + count + " points");
-            System.out.println(luckynumber);
+        System.out.println("Guess the number from " + low + " to " + high + " in maximum chance " + maxChance);
 
 
+        while (count < maxChance) {
+            System.out.println("guess the number");
+            number = input.nextInt();
+            count++;
+
+            if (number == luckynumber) {
+                System.out.println("CONGRATULATIONS ! You found the number");
+            } else if (number > luckynumber) {
+                System.out.println("The number is smaller than you guessed");
+            } else {
+                System.out.println("The number is bigger than you guessed");
+            }
         }
 
-
+        if (count == maxChance) {
+            System.out.println("You don't have more chance");
+        }
+        System.out.println("Luck number is : " + luckynumber);
 
 
     }
+
+}
 
 
 
